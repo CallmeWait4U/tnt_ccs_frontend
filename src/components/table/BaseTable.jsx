8a24f773435cffbff25e2fcaf003eb52a46d6a3c
Huy Ngo <www.huyngo@gmail.com>
2023-11-30 10:27:21 +0700

    quan ly khach hang

---
+++ src/components/table/BaseTable.jsx
@@ -0,0 +1,50 @@
+import { Table } from "antd";
+import { useEffect, useState } from "react";
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      "selectedRows: ",
+      selectedRows
+    );
+  },
+  getCheckboxProps: (record) => ({
+    disabled: record.name === "Disabled User",
+    // Column configuration not to be checked
+    name: record.name,
+  }),
+};
+const BaseTable = ({ data, columns, onRow }) => {
+  const [selectionType, setSelectionType] = useState("checkbox");
+  const [tableParams, setTableParams] = useState({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+  });
+
+  const handleTableChange = (pagination, filters, sorter) => {
+    setTableParams({
+      pagination,
+      filters,
+      ...sorter,
+    });
+
+    // `dataSource` is useless since `pageSize` changed
+  };
+  return (
+    <Table
+      rowSelection={{
+        type: selectionType,
+        ...rowSelection,
+      }}
+      onRow={onRow}
+      columns={columns}
+      dataSource={data}
+      pagination={tableParams.pagination}
+      className="ant-border-space"
+      onChange={handleTableChange}
+    />
+  );
+};
+export default BaseTable;
